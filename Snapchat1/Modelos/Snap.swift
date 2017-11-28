c707213ba64703546742ddba23b341a2a4f17c3a
Mac Tecsup <linda.ugarte@tecsup.edu.pe>
2017-11-27 20:13:36 -0500

    creando los snaps en firebase

---
+++ Snapchat1/Modelos/Snap.swift
@@ -0,0 +1,15 @@
+//
+//  Snap.swift
+//  Snapchat1
+//
+//  Created by Mac Tecsup on 27/11/17.
+//  Copyright © 2017 Tecsup. All rights reserved.
+//
+
+import Foundation
+class Snap{
+    var imagenURL = ""
+    var descrip = ""
+    var from = ""
+    
+}
--- Snapchat1/ViewControllers/SnapsViewController.swift
+++ Snapchat1/ViewControllers/SnapsViewController.swift
@@ -7,17 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
-class SnapsViewController: UIViewController {
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var snaps : [Snap] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        Database.database().reference().child("usuarios").child(Auth.auth().currentUser!.uid).child("snaps").observe(DataEventType.childAdded, with: {(snaphot)in
+            
+        let snap = Snap()
+        
+            snap.imagenURL = (snaphot.value as! NSDictionary)["imagenURL"] as! String
+            snap.from = (snaphot.value as! NSDictionary)["from"] as! String
+            snap.descrip = (snaphot.value as! NSDictionary)["descripcion"] as! String
+        self.snaps.append(snap)
+        self.tableView.reloadData()
+        
+        })
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+            let cell = UITableViewCell()
+            let snap = snaps[indexPath.row]
+            cell.textLabel?.text = snap.from
+            return cell
+        }
+    
+
     @IBAction func cerrarSesionTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
 }
